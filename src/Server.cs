e12b9c0f7b50eaec93b6c64e36990c19e2e0bcab
burhansavci <burhansavci@gmail.com>
2024-07-19 23:40:00 +0300

    send 200 OK

--- src/Server.cs
+++ src/Server.cs
@@ -9,5 +9,5 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
-var message = $"HTTP/1.1 200 OK{Environment.NewLine}{Environment.NewLine}";
+var message = "HTTP/1.1 200 OK\r\n\r\n";
 socket.Send(Encoding.UTF8.GetBytes(message));
