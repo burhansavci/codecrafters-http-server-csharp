edb20312efdf3674fe888fe17a9f0abc19251710
burhansavci <burhansavci@gmail.com>
2024-07-19 23:36:14 +0300

    CLI tests

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -8,3 +9,5 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 server.AcceptSocket(); // wait for client
+var message = $"HTTP/1.1 200 OK{Environment.NewLine}{Environment.NewLine}";
+server.Server.Send(Encoding.UTF8.GetBytes(message));
