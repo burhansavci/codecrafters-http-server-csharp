0983e93c4de43e8e7cf4a5293d2bbd5ede3c325f
burhansavci <burhansavci@gmail.com>
2024-07-20 14:33:04 +0300

    Respond with body

--- src/Server.cs
+++ src/Server.cs
@@ -2,10 +2,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-const string httpOkMessage = "HTTP/1.1 200 OK\r\n\r\n";
-const string httpNotFoundMessage = "HTTP/1.1 404 Not Found\r\n\r\n";
-const string rootPath = "/";
-
 var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Console.WriteLine("Server started");
@@ -21,6 +17,43 @@ var message = Encoding.UTF8.GetString(buffer[..read]);
 var requestLine = message.Split("\r\n")[0];
 var requestTarget = requestLine.Split(" ")[1];
 
-var responseMessage = requestTarget == rootPath ? httpOkMessage : httpNotFoundMessage;
+var content = requestTarget.Split("/").Last();
+var headers = new Dictionary<string, string>
+{
+    { "Content-Type", "text/plain" },
+    { "Content-Length", content.Length.ToString() }
+};
+var responseMessage = new HttpResponse(HttpStatus.Ok, content, headers).ToString();
 
 socket.Send(Encoding.UTF8.GetBytes(responseMessage));
+
+record HttpResponse(HttpStatus Status, string Content, Dictionary<string, string> Headers)
+{
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append("HTTP/1.1 ");
+        sb.Append(Status);
+        sb.Append("\r\n");
+
+        foreach (var (key, value) in Headers)
+        {
+            sb.Append(key);
+            sb.Append(": ");
+            sb.Append(value);
+            sb.Append("\r\n");
+        }
+
+        sb.Append("\r\n");
+        sb.Append(Content);
+
+        return sb.ToString();
+    }
+}
+
+
+record HttpStatus(int Code, string ReasonPhrase)
+{
+    public static HttpStatus Ok => new(200, "OK");
+    public override string ToString() => $"{Code} {ReasonPhrase}";
+}
